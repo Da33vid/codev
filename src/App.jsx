f8fa0cea39a0e3c1f55e119b8dd61d5667d1a1a0
Da33vid <123070526+Da33vid@users.noreply.github.com>
2023-12-12 19:54:51 +0100

    cm

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import miFoto from './assets/fotoMia.png'
+import logoTikTok from './assets/tiktok.png'
+import logoInstagram from './assets/instagram.png'
+import logoGithub from './assets/github.png'
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <img src={miFoto} className="foto" alt="Mi foto" />
+      <h1>David Aparicio López</h1>
+      <h2>Desarrollador Web</h2>
+      <div className="logos">
+        <a href="https://www.tiktok.com/@codev.jr" target="_blank">
+          <img src={logoTikTok} className="logo" alt="Logo de TikTok" />
+        </a>
+        <a href="https://www.instagram.com/codev.jr" target="_blank">
+          <img src={logoInstagram} className="logo" alt="Logo de Instagram" />
+        </a>
+        <a href="https://github.com/Da33vid" target="_blank">
+          <img src={logoGithub} className="logo" alt="Logo de Github" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
